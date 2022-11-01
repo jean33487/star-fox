a5ce071f487dcaf9532c804d40a89707e5143a00
GolfBravoSierra <106974254+GolfBravoSierra@users.noreply.github.com>
2022-11-01 12:27:11 -0300

    01/11/22
    arrumei os controller e só

--- app/Http/Controllers/alunos.php
+++ app/Http/Controllers/alunos.php
@@ -4,7 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Model\Aluno;
+
 class alunos extends Controller
 {
-    //
+    public function index(){
+
+        $alunos = Aluno::all();
+
+        return view('welcome',['alunos' => $alunos])
+    }
 }
