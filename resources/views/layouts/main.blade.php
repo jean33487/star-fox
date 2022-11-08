0b2dc53980238811accc5a57d570ba318744f020
GolfBravoSierra <106974254+GolfBravoSierra@users.noreply.github.com>
2022-11-08 11:22:45 -0300

    mainblade
    agora nosso site tem main blade para não precisar repetir os layouts das paginas (tem alguns cometarios)
    ja tem a pagina inicial e professores

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>@yield('title')</title>
+    </head>
+        <body>
+
+            @yield('content')
+
+            <footer>
+                <p>Star-Fox  school &copy; 2022</p>
+            </footer>
+        </body>
+</html>
+
