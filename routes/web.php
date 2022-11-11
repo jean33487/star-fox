52a7c49bca75381f25be7e558ab88e8e98e7ae13
GolfBravoSierra <106974254+GolfBravoSierra@users.noreply.github.com>
2022-11-11 09:05:01 -0300

    11/11/2022

--- routes/web.php
+++ routes/web.php
@@ -13,35 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-use App\Http\Controllers\Cordenacao;
-use App\Http\Controllers\Aluno;
-use App\Http\Controllers\Adm;
-use App\Http\Controllers\Professor;
-
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/alunos', function () {
-    return view('alunos');
-});
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/cordenação', function () {
-    return view('cordenação');
-});
-
-Route::get('/professores', function () {
-    return view('professores');
-});
-
-Route::get('/area_do_aluno/{id}', function ($id = null) {
-    return view('area_do_aluno',[id => $id]);
-});
-
-Route::get('/area_do_professor/{id}', function ($id = null) {
-    return view('area_do_professor',[id => $id]);
-});
-
-Route::get('/area_da_cordenação/{id}', function ($id = null) {
-    return view('area_da_cordenação',[id => $id]);
-});
+require __DIR__.'/auth.php';
