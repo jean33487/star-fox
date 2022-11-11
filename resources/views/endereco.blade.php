06d70f49f879579dca26e232c6ebc6fb2fd2cedc
Jean Piton <jean.geada1@gmail.com>
2022-11-11 09:35:17 -0300

    CEP validação tamanho/caracteres

--- resources/views/endereco.blade.php
+++ resources/views/endereco.blade.php
@@ -1,10 +1,21 @@
 <!DOCTYPE html>
 <!--<! -- Trazer Resultados CEP, Rua, Bairro e Estado -->
 <?php
-    if( isset ($_POST['cep']))
-    $cep = $_POST['cep'];
-    $url = "https://viacep.com.br/ws/{$cep}/json/"
-    $address = json_decode(file_get_contents($url));
+    $address = (object)[
+        'cep' => '',
+        'logradouro' => '',
+        'bairro' => '',
+        'uf' => ''
+    ];
+
+    if( isset ($_POST['cep'])){
+        $cep = $_POST['cep'];
+        $cep = preg_replace('/[^0-9]/','',$cep);
+        if(preg_match('/^[0-9]{5}-?[0-9]{3}$/',$cep)){
+            $url = "https://viacep.com.br/ws/{$cep}/json/";
+            $address = json_decode(file_get_contents($url));
+        }
+    }
 ?>
 <html lang="pt-br"> <!-- Declaração do Idioma  -->
     <head>
@@ -14,14 +25,14 @@
 <form action="" method="post">  
     @csrf      
     <label> Insira o CEP: </label>
-    <input type="text" name="cep"> 
+    <input type="text" name="cep" value=<?php echo $address->cep ?> > 
     <input type="submit" value="Enviar"> <br />
     <label>Rua:
-        <input name="rua" type="text" id="rua" size="60" value="<?php echo $address->logradouro ?? '' ?>"/></label><br />
+        <input name="rua" type="text" id="rua" size="60" value="<?php echo $address->logradouro ?>"/></label><br />
     <label>Bairro:
-        <input name="bairro" type="text" id="bairro" size="40" value="<?php echo $address->bairro ?? '' ?>" /></label><br />
+        <input name="bairro" type="text" id="bairro" size="40" value="<?php echo $address->bairro ?>" /></label><br />
     <label>Estado:
-        <input name="uf" type="text" id="uf" size="2" value="<?php echo $address->uf ?? '' ?>" /></label><br />
+        <input name="uf" type="text" id="uf" size="2" value="<?php echo $address->uf ?>" /></label><br />
 </form>
     
 </body>
