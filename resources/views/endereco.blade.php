6fc0c5eb9452ace2a9a9ade94f11a286b83883af
Jean Piton <jean.geada1@gmail.com>
2022-11-12 12:20:51 -0300

    terminei o cep

--- resources/views/endereco.blade.php
+++ resources/views/endereco.blade.php
@@ -1,21 +1,44 @@
 <!DOCTYPE html>
 <!--<! -- Trazer Resultados CEP, Rua, Bairro e Estado -->
 <?php
-    $address = (object)[
-        'cep' => '',
-        'logradouro' => '',
-        'bairro' => '',
-        'uf' => ''
-    ];
-
-    if( isset ($_POST['cep'])){
-        $cep = $_POST['cep'];
-        $cep = preg_replace('/[^0-9]/','',$cep);
-        if(preg_match('/^[0-9]{5}-?[0-9]{3}$/',$cep)){
-            $url = "https://viacep.com.br/ws/{$cep}/json/";
-            $address = json_decode(file_get_contents($url));
+    function getAddress(){
+        if( isset ($_POST['cep'])){
+            $cep = $_POST['cep'];
+            $cep = filterCep($cep);
+            if(isCep($cep)){
+                $address = getAddressViaCep($cep);
+                if(property_exists($address,'erro')){
+                    $address = addressEmpty();
+                    $address->cep = 'CEP não encontrado!';    
+                }
+            }else{
+                $address = addressEmpty();
+                $address->cep = 'CEP inválido!';
+            }
+        }else{
+            $address = addressEmpty();
         }
+        return $address;
     }
+    function addressEmpty(){
+        return (object)[
+            'cep' => '',
+            'logradouro' => '',
+            'bairro' => '',
+            'uf' => ''
+        ];
+    }
+    function filterCep(String $cep):String{
+        return preg_replace('/[^0-9]/','',$cep); 
+    }
+    function isCep(String $cep):bool{
+        return preg_match('/^[0-9]{5}-?[0-9]{3}$/',$cep);
+    }
+    function getAddressViaCep(String $cep){
+        $url = "https://viacep.com.br/ws/{$cep}/json/";
+        return json_decode(file_get_contents($url));
+    }
+    $address = getAddress();
 ?>
 <html lang="pt-br"> <!-- Declaração do Idioma  -->
     <head>
@@ -25,7 +48,7 @@
 <form action="" method="post">  
     @csrf      
     <label> Insira o CEP: </label>
-    <input type="text" name="cep" value=<?php echo $address->cep ?> > 
+    <input type="text" name="cep" value="<?php echo $address->cep ?>" > 
     <input type="submit" value="Enviar"> <br />
     <label>Rua:
         <input name="rua" type="text" id="rua" size="60" value="<?php echo $address->logradouro ?>"/></label><br />
