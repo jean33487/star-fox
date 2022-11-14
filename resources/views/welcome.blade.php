9719bde7b4c02c6f07825e59e4b7b818f23d205b
GolfBravoSierra <106974254+GolfBravoSierra@users.noreply.github.com>
2022-11-14 15:31:35 -0300

    14/11/2022
    só um pqueno ajuste no .env pq ele tava funcioando para alunos e agora não ta mais, porem eu arrumei a agora ta funcionando só o de professsores ainda não ta

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
 
 @section('content')
     <h1>tela inicila para escolher quem vc é ;professor aluno ou da cordenação</h1>
+    <p>para retornar a essa pagina em qualquer lugar é só clicar no logo</p>
 
 
 @endsection
