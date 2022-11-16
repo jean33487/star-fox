9768162d1eed8d93ea07c40f1b1b5a9b8dc781de
GolfBravoSierra <106974254+GolfBravoSierra@users.noreply.github.com>
2022-11-16 16:46:27 -0300

    16/11/2022
    Filme favorito adicionado
    Erro default

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -36,12 +36,15 @@ class RegisteredUserController extends Controller
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'filme' => ['required', 'string', 'max:255'],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
+        
         ]);
 
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
+            'filme' => $request->filme,
             'password' => Hash::make($request->password),
         ]);
 
