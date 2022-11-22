40b7dafb92a99aa14f69ca33def63c7085a8e121
Jean Piton <jean.geada1@gmail.com>
2022-11-22 11:16:30 -0300

    mudou ordem casa

--- resources/views/endereco.blade.php
+++ resources/views/endereco.blade.php
@@ -11,14 +11,14 @@
     <label> Insira o CEP: </label>
     <input type="text" name="cep" value="<?php echo $address->cep ?>"> 
     <input type="submit" value="Buscar"> <br />
-    <label>Casa:
-        <input name="casa" type="text" id="casa" size="4"/></label><br/>
     <label>Rua:
         <input name="rua" type="text" id="rua" size="60" value="<?php echo $address->logradouro ?>" readonly/></label><br />
     <label>Bairro:
         <input name="bairro" type="text" id="bairro" size="40" value="<?php echo $address->bairro ?>" readonly/></label><br />
     <label>Estado:
         <input name="uf" type="text" id="uf" size="2" value="<?php echo $address->uf ?>" readonly/></label><br />
+    <label>Casa:
+        <input name="casa" type="text" id="casa" size="4"/></label>
 </form>
     
 </body>
