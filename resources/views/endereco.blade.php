a41ffb41e011486eb01f5b6bc32c6173f31336e6
Jean Piton <jean.geada1@gmail.com>
2022-11-22 10:49:03 -0300

    criação do helpers.php

--- resources/views/endereco.blade.php
+++ resources/views/endereco.blade.php
@@ -1,45 +1,6 @@
 <!DOCTYPE html>
 <!--<! -- Trazer Resultados CEP, Rua, Bairro e Estado -->
-<?php
-    function getAddress(){
-        if( isset ($_POST['cep'])){
-            $cep = $_POST['cep'];
-            $cep = filterCep($cep);
-            if(isCep($cep)){
-                $address = getAddressViaCep($cep);
-                if(property_exists($address,'erro')){
-                    $address = addressEmpty();
-                    $address->cep = 'CEP não encontrado!';    
-                }
-            }else{
-                $address = addressEmpty();
-                $address->cep = 'CEP inválido!';
-            }
-        }else{
-            $address = addressEmpty();
-        }
-        return $address;
-    }
-    function addressEmpty(){
-        return (object)[
-            'cep' => '',
-            'logradouro' => '',
-            'bairro' => '',
-            'uf' => ''
-        ];
-    }
-    function filterCep(String $cep):String{
-        return preg_replace('/[^0-9]/','',$cep); 
-    }
-    function isCep(String $cep):bool{
-        return preg_match('/^[0-9]{5}-?[0-9]{3}$/',$cep);
-    }
-    function getAddressViaCep(String $cep){
-        $url = "https://viacep.com.br/ws/{$cep}/json/";
-        return json_decode(file_get_contents($url));
-    }
-    $address = getAddress();
-?>
+<?php $address=getAddress() ?>
 <html lang="pt-br"> <!-- Declaração do Idioma  -->
     <head>
         <meta charset="UTF-8">
@@ -48,14 +9,16 @@
 <form action="" method="post">  
     @csrf      
     <label> Insira o CEP: </label>
-    <input type="text" name="cep" value="<?php echo $address->cep ?>" > 
-    <input type="submit" value="Enviar"> <br />
+    <input type="text" name="cep" value="<?php echo $address->cep ?>"> 
+    <input type="submit" value="Buscar"> <br />
+    <label>Casa:
+        <input name="casa" type="text" id="casa" size="4"/></label><br/>
     <label>Rua:
-        <input name="rua" type="text" id="rua" size="60" value="<?php echo $address->logradouro ?>"/></label><br />
+        <input name="rua" type="text" id="rua" size="60" value="<?php echo $address->logradouro ?>" readonly/></label><br />
     <label>Bairro:
-        <input name="bairro" type="text" id="bairro" size="40" value="<?php echo $address->bairro ?>" /></label><br />
+        <input name="bairro" type="text" id="bairro" size="40" value="<?php echo $address->bairro ?>" readonly/></label><br />
     <label>Estado:
-        <input name="uf" type="text" id="uf" size="2" value="<?php echo $address->uf ?>" /></label><br />
+        <input name="uf" type="text" id="uf" size="2" value="<?php echo $address->uf ?>" readonly/></label><br />
 </form>
     
 </body>
