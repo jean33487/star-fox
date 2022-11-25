eac652f6c8fa1029de90490f183591067b7a0606
MatheusZCAR <matheuscarita@gmail.com>
2022-11-25 20:57:53 -0300

    Materia consetada

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        Schema::defaultStringLength(255);
+       
     }
 
     /**
@@ -24,6 +24,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Schema::defaultStringLength(255);
     }
 }
