09b0c111e48fb9de2380ec012efc14df2bddd6c0
MatheusZCAR <matheuscarita@gmail.com>
2022-11-26 00:08:08 -0300

    Consertando Erros de segurança de login
    Bugs de Login, Logout e cadastro consertados!

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -9,6 +9,8 @@ use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
+use Illuminate\Http\Request;
+
 class RegisterController extends Controller
 {
     /*
@@ -70,21 +72,63 @@ class RegisterController extends Controller
      * @param  array  $data
      * @return \App\Models\User
      */
-    protected function create(array $data)
-    {
-        return User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'role'=>2,
-            'UsuarioOficial'=>$data['UsuarioOficial'],
-            'FilmeFavorito'=>$data['FilmeFavorito'],
-            'cep'=>$data['cep'],
-            'rua'=>$data['rua'],
-            'bairro'=>$data['bairro'],
-            'uf'=>$data['uf'],
-            'cidade'=>$data['cidade'],
-            'CPF'=>$data['CPF'],
-            'password' => Hash::make($data['password']),
+    //protected function create(array $data)
+    //{
+    //    return User::create([
+    //        'name' => $data['name'],
+    //        'email' => $data['email'],
+    //        'role'=>2,
+    //         'UsuarioOficial'=>$data['UsuarioOficial'],
+    //         'FilmeFavorito'=>$data['FilmeFavorito'],
+    //         'cep'=>$data['cep'],
+    //         'rua'=>$data['rua'],
+    //         'bairro'=>$data['bairro'],
+    //         'uf'=>$data['uf'],
+    //         'cidade'=>$data['cidade'],
+    //         'CPF'=>$data['CPF'],
+    //         'password' => Hash::make($data['password']),
+    //    ]);
+    // }
+
+    function register(Request $request){
+
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'UsuarioOficial' =>'required',
+            'FilmeFavorito' => 'required',
+            'cep' =>'required',
+            'rua' =>'required',
+            'bairro' =>'required',
+            'uf' =>'required',
+            'cidade' =>'required',
+            'CPF' =>'required',
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+
         ]);
+
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->role = $request->role;
+        $user->UsuarioOficial = $request->UsuarioOficial;
+        $user->FilmeFavorito = $request->FilmeFavorito;
+        $user->cep = $request->cep;
+        $user->rua = $request->rua;
+        $user->bairro = $request->bairro;
+        $user->uf = $request->uf;
+        $user->cidade = $request->cidade;
+        $user->CPF = $request->CPF;
+        $user->password = $request->password;
+
+        if( $user->save() ){
+
+            return redirect()->back()->with('success','Cadastrado com sucesso!');
+        }else{
+            return redirect()->back()->with('error','Falha ao realizar o cadastro');
+        }
     }
+
+
+
 }
